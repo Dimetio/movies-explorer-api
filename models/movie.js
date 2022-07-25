457a07674175f0c7bfdb217e2eee12bcf8c44280
ddubinin <dubinin.dmitry@mail.ru>
2022-07-25 22:12:55 +0300

    feat: create movie schema

---
+++ models/movie.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose');
+const isUrl = require('validator/lib/isURL');
+
+const modieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: [true, 'Обазательное поле'],
+  },
+  director: {
+    type: String,
+    required: [true, 'Обазательное поле'],
+  },
+  duration: {
+    type: Number,
+    required: [true, 'Обазательное поле'],
+  },
+  year: {
+    type: String,
+    required: [true, 'Обазательное поле'],
+  },
+  description: {
+    type: String,
+    required: [true, 'Обазательное поле'],
+  },
+  image: {
+    type: String,
+    required: [true, 'Обазательное поле'],
+    validate: {
+      validator: (url) => isUrl(url),
+      message: 'Некорректный url',
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: [true, 'Обазательное поле'],
+    validate: {
+      validator: (url) => isUrl(url),
+      message: 'Некорректный url',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: [true, 'Обазательное поле'],
+    validate: {
+      validator: (url) => isUrl(url),
+      message: 'Некорректный url',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: [true, 'Обазательное поле'],
+    ref: 'user',
+  },
+  movieId: {
+    type: String,
+    required: [true, 'Обазательное поле'],
+  },
+  nameRU: {
+    type: String,
+    required: [true, 'Обазательное поле'],
+  },
+  nameEN: {
+    type: String,
+    required: [true, 'Обазательное поле'],
+  },
+});
+
+module.exports = mongoose.model('movie', modieSchema);
