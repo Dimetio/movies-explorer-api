19c489389c1304f33735594a9fdf02e49e0b3055
ddubinin <dubinin.dmitry@mail.ru>
2022-07-26 20:30:51 +0300

    feat: init new app

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const handleError = require('./middlewares/handleError');
+const router = require('./routes');
 const NotFoundError = require('./errors/NotFoundError');
 
 const { PORT = 3000, DB_CONNECT = 'mongodb://localhost:27017/bitfilmsdb' } = process.env;
 
 const app = express();
+
 const options = {
   origin: [
     'http://localhost:3000',
@@ -17,14 +22,17 @@ const options = {
 };
 app.use('*', cors(options)); // доступность cors
 
+app.use(cookieParser()); // куки
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect(DB_CONNECT); // подключение к базе
 
-app.use(requestLogger);
+app.use(requestLogger); // логгер запросов
 
-app.use(errorLogger);
+app.use('/', router); // все роуты
+
+app.use(errorLogger); // логгре запросов с ошибками
 
 app.all('*', (req, res, next) => {
   next(new NotFoundError('Неправильный путь. Error 404'));
@@ -32,17 +40,6 @@ app.all('*', (req, res, next) => {
 
 app.use(errors());
 
-// eslint-disable-next-line no-unused-vars
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-
-  res
-    .status(statusCode)
-    .send({
-      message: statusCode === 500
-        ? 'На сервере произошла ошибка'
-        : message,
-    });
-});
+app.use(handleError);
 
 app.listen(PORT);
