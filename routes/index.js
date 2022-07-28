253536a945be6877669b8db9a592324119084400
ddubinin <dubinin.dmitry@mail.ru>
2022-07-29 00:26:05 +0300

    fix: optimization

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,12 @@ const NotFoundError = require('../errors/NotFoundError');
 
 router.post('/signup', signupValidate, createUser);
 router.post('/signin', signinValidate, signin);
-router.use('/users', auth, usersRoutes);
-router.use('/movies', auth, moviesRoutes);
-router.post('/signout', signout);
 
-router.use('*', auth);
+router.use('*', auth); // на все ройты авторизацию
+
+router.use('/users', usersRoutes);
+router.use('/movies', moviesRoutes);
+router.post('/signout', signout);
 
 router.use('*', (req, res, next) => {
   next(new NotFoundError('Неправильный путь. Error 404'));
