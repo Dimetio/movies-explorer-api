4a9e43c6cc07efa026dff833f293dafe043117e2
ddubinin <dubinin.dmitry@mail.ru>
2022-07-28 17:31:56 +0300

    fix: fix routes

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 const router = require('express').Router();
-const { login, createUser, logout } = require('../controllers/users');
+const { signin, createUser, signout } = require('../controllers/users');
 const usersRoutes = require('./users');
 const moviesRoutes = require('./movies');
 const auth = require('../middlewares/auth');
 const { signupValidate, signinValidate } = require('../middlewares/joi-schemas');
 
 router.use('/users', auth, usersRoutes);
-router.use('/movie', auth, moviesRoutes);
+router.use('/movies', auth, moviesRoutes);
 router.post('/signup', signupValidate, createUser);
-router.post('/signin', signinValidate, login);
-router.post('/logout', logout);
+router.post('/signin', signinValidate, signin);
+router.post('/signout', signout);
+
+router.all('*', auth);
 
 module.exports = router;
