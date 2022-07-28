72abc8a4f8deab5a43c3a8beaec119ddf5d2dc47
ddubinin <dubinin.dmitry@mail.ru>
2022-07-29 00:06:42 +0300

    feat: add route 404 error

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,17 @@ const usersRoutes = require('./users');
 const moviesRoutes = require('./movies');
 const auth = require('../middlewares/auth');
 const { signupValidate, signinValidate } = require('../middlewares/joi-schemas');
+const NotFoundError = require('../errors/NotFoundError');
 
-router.use('/users', auth, usersRoutes);
-router.use('/movies', auth, moviesRoutes);
 router.post('/signup', signupValidate, createUser);
 router.post('/signin', signinValidate, signin);
 router.post('/signout', signout);
+router.use('/users', auth, usersRoutes);
+router.use('/movies', auth, moviesRoutes);
+
+router.all('*', (req, res, next) => {
+  next(new NotFoundError('Неправильный путь. Error 404'));
+});
 
 router.all('*', auth);
 
